6cc8cf1c01d195f893c69054f37eb2aec24fa3e2
Marie Caron <marie.caron@epitech.eu>
2025-02-10 19:19:30 +0100

    feat: creation program debogage.

---
+++ ex_04.js
@@ -0,0 +1,10 @@
+<html>
+<body>
+  <p>Welcome to the javascript pool!</p>
+  <p>Have some fun and dont forget to bring a towel.</p>
+<script>
+console.log("Website launched!");
+</script>
+
+</body>
+</html>
